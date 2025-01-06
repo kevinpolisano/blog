7209ba6f77777451d67cc1ca192157c194c27931
Kevin Polisano <kevin.polisano@univ-grenoble-alpes.fr>
2025-01-06 21:52:43 +0100

    add 2016-03-03-creer-une-documentation-etou-une-toolbox-dans-matlab

---
+++ posts/2016-03-03-creer-une-documentation-etou-une-toolbox-dans-matlab/doc-matlab-exemple/index.m
@@ -0,0 +1,18 @@
+%% Convex Super-Resolution Detection of Lines in Images
+
+%% Main functions
+
+%%
+% * <data_generation.html Data Generation>
+% * <minimization_Chambolle.html Minimization Algorithm>
+% * <resynthesis.html Resynthesis>
+% * <prony.html Prony Estimation>
+
+%% Tests
+
+%%
+% * <noiseless.html Experiment 1 : normal blur and noiseless>
+% * <strong_noise.html Experiment 2 : normal blur and strong noise>
+% * <strong_blur.html Experiment 3 : strong blur and noiseless>
+% * <hough_lines_detection.html Hough lines detection>
+% * <radon_lines_detection.html Radon lines detection>
